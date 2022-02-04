e6773e84c76bc0c5d6bd95575d9b8feae47f04c6
Bernardo A. Dal Corno (c082647) <c082647@mail.caixa>
2022-02-04 16:05:33 -0300

    with filter

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('clarify')
 const StackTracey = require('stacktracey')
 
-module.exports = function (e, logger) {
-  const trace = new StackTracey(e).items.map(c => `at ${c.calleeShort || '???'} ${c.fileRelative}:${c.line}:${c.column}`)
+function filter (a, F) {
+  if (!F) return a
+
+  return a.filter(c => !F.some(function (f) {
+    if (f instanceof RegExp) return f.test(c.fileRelative)
+    else return c.fileRelative.includes(f)
+  }))
+}
+
+module.exports = function (e, logger, withFilter) {
+  const trace = filter(new StackTracey(e).items, withFilter).map(c => `at ${c.calleeShort || '???'} ${c.fileRelative}:${c.line}:${c.column}`)
   const stack = `${e.message}\n  ${trace.join('\n  ')}`
 
   if (logger) logger(stack)
