ceccfb86cc37e3fbed878393457af2c5793361f6
Bernardo A. Dal Corno (c082647) <c082647@mail.caixa>
2022-02-04 15:48:07 -0300

    correction

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const stackTrace = require('stack-trace')
 
 module.exports = function (e, logger) {
-  const trace = stackTrace.parse(e).map(c => `at ${c.getMethodName() || c.getFunctionName() || '<anonymous>'} ${c.getFileName()}:${c.getLineNumber()}:${c.getColumnNumber()}`)
-  const stack = `${e.message}\n  ${trace.join('\n  ')}`
-  if (logger) logger(stack)
+  let trace
 
+  const map = c => `at ${c.getMethodName() || c.getFunctionName() || '<anonymous>'} ${c.getFileName()}:${c.getLineNumber()}:${c.getColumnNumber()}`
+  if (e) trace = stackTrace.parse(e).map(map)
+  else trace = stackTrace.get().map(map)
+  const stack = `${e.message}\n  ${trace.join('\n  ')}`
+
+  if (logger) logger(stack)
   return stack
 }
